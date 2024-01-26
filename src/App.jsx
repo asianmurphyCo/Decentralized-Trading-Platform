adc17c1822dae1023e243e1e314d6c8b8ba5e626
PancakesLmao <phucthin29@gmail.com>
2024-01-27 00:46:27 +0700

    Added a small feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './components/assets/react.svg'
 import viteLogo from '/vite.svg'
+import assistant from './components/assets/kiana-kiana-wave.gif'
 import './App.css'
 import Header from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
+  // add 0.2s delay before pop up the chatbox
+  const showchat = () => {
+    setTimeout(() => {
+      document.getElementById('chatbox').style.display = 'block';
+    }, 200);  
+  };
 
+  const hidechat = () => {
+    document.getElementById('chatbox').style.display = 'none';
+  };
   return (
     <>
       <Header/>
@@ -25,7 +35,12 @@ function App() {
         </button>
       </div> */}
       
-      
+      <div className='assist-sector bg-transparent'>
+          <img src={assistant} alt='assistant' className='assistant'onMouseOver={showchat} onMouseOut={hidechat}/>
+      </div>
+      <div className='notification' id='chatbox'>
+        <p className='card-text'>Need help? Chat with Tuna now!</p>
+      </div>
     </>
   )
 }
---
+++ src/components/mainpage.jsx
@@ -0,0 +1,10 @@
+// import
+function Main() {
+    return (
+        <div>
+            
+        </div>
+    )
+
+}
+export default Main
