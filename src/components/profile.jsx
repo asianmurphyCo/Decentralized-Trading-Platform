79c8347b24d88d001966ccadbaf503101d13b3ae
PancakesLmao <phucthin29@gmail.com>
2024-02-03 14:09:24 +0700

    fixed login logout

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -33,44 +33,6 @@ function Profile() {
   }, []);
 
   // Check if user is logged in
-  if (!isLoggedIn) {
-    // Render an error message or a login prompt
-    return (
-      <div className="error-message vh-100">
-        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
-          <div className="container h-100">
-            <div className="col-sm-12 col-md-12 col-lg-12 mb-5 mt-5">
-              <div className="card login-bg">
-                <p className="text-uppercase text-center mb-4">
-                  You are not logged in. Please log in to view this page.
-                </p>
-                {/* DIRECT USER BACK TO LOGIN PAGE */}
-                <img className="w-30 mx-auto d-block" src={cat} alt="getout" />
-                <button>
-                  <Link to="/login" className="text-light">
-                    Go back to log in&nbsp;
-                  </Link>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    width="25"
-                    height="25"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   if (!userData && isLoggedIn) {
     // If userData is still null, return a loading page
     return (
