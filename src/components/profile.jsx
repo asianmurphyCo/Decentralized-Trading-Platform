fc1fc4b9ca1574f27535c0330471cf996ae8720f
PancakesLmao <phucthin29@gmail.com>
2024-02-03 13:56:28 +0700

    fixed login logout

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,13 +1,15 @@
 // import
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./css/style.css";
 import profile_pic from "../components/assets/profile.png";
 import cat from "../components/assets/mya-thurston-waffles.gif";
+import hertaload from "../components/assets/herta.webp";
 import { CircularProgress, Box } from "@mui/material";
 
-// PASS Username key from Local Storage
+// PASS Username key and login state from Local Storage
 const Username = localStorage.getItem("username");
+const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
 // DEBUG LINE
 console.log(Username);
 
@@ -30,21 +32,32 @@ function Profile() {
     fetchData();
   }, []);
 
-  if (!userData) {
+  if (!userData && isLoggedIn) {
     // If userData is still null, return a loading page
     return (
       <div>
-        <Box sx={{ display: "flex", alignContent: "center" }}>
-          <CircularProgress />
-        </Box>
+        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+          <div className="container h-100">
+            <div className="col-sm-12 col-md-12 col-lg-12 mb-5 mt-5">
+              <div className="card login-bg">
+                {/* DIRECT USER BACK TO LOGIN PAGE */}
+                <img
+                  className="w-30 mx-auto d-block"
+                  src={hertaload}
+                  alt="getout"
+                  style={{ width: "20%" }}
+                />
+                <h1 className="text-center mb-4">Loading...</h1>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 
   // Check if user is logged in
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-
-  if (!isLoggedIn) {
+  if (!isLoggedIn && !userData) {
     // Render an error message or a login prompt
     return (
       <div className="error-message vh-100">
