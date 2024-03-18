b44a0a0d1256921edc1f0937451ffa232b9af6e3
PancakesLmao <phucthin29@gmail.com>
2024-03-18 22:41:31 +0700

    set json

--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ app.get('/api', (req, res) => {
 // Route for fetching coin list
 app.get('/database', async (req, res) => {
   const coinlist = await dashboard.getCoinList();
-  res.status(200).send(coinlist);
+  console.log(json(coinlist))
+  res.status(200).json(coinlist);
 });
 
 app.post("/authenticate", authenticate);
